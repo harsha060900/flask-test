327cdd4eb6313a96908fca5ec2039f50d128d18b
harsha060900 <harshamanoharsha@gmail.com>
2024-03-06 09:15:52 +0530

    get done

--- src/category/views.py
+++ src/category/views.py
@@ -5,10 +5,10 @@ import uuid
 
 def list_all():
     data= Category.query.all()
-    print('data:', data)
+    print('data:', data[0].cate_name)
     response = []
-    for x in data: response.append(x)
-    return response
+    for x in data: response.append({'id':x.id, 'cateName':x.cate_name, 'createdAt':x.created, 'updatedAt':x.updated})
+    return jsonify(response)
 
 def createCate():
     data = request.form.to_dict()
