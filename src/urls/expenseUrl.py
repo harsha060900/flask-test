aecc30fa0d321c0e7bcbcf9cc3d50c820d970b9b
harshavardhan-ngp <harshavardhan@ngpwebsmart.com>
2024-05-15 20:02:41 +0530

    url update

--- src/urls/expenseUrl.py
+++ src/urls/expenseUrl.py
@@ -1,8 +1,14 @@
 from flask import request
 from ..app import app
-from ..views.expenseView import addExpense,listExpense
+from ..views.expenseView import addExpense,listExpense,deleteExpense
 
 @app.route("/expense", methods=['GET','POST'])
 def exp():
     if request.method=='GET' : return listExpense()
     if request.method=='POST' : return addExpense()
+
+@app.route("/expense/<paramId>", methods=['GET','POST'])
+def exp(paramId):
+    if request.method=='GET' : return deleteExpense(paramId)
+    # if request.method=='POST' : return addExpense()
+
