d50e1e1c1c5f557b042dfbe28714f2464614ab4f
harshavardhan-ngp <harshavardhan@ngpwebsmart.com>
2024-05-30 19:59:29 +0530

    balance add, list

--- src/urls/userUrl.py
+++ src/urls/userUrl.py
@@ -1,9 +1,10 @@
 from flask import request
 from ..app import app
 from ..models.UserModel import User
+from ..views.UserView import listUser, addBalance
 
 @app.route("/user", methods=['GET','POST'])
 def user():
     pass
-    if request.method=='GET' : return User.query.all() 
-    # if request.method=='POST' : return addExpense()
+    if request.method=='GET' : return listUser() 
+    if request.method=='POST' : return addBalance()
--- src/views/UserView.py
+++ src/views/UserView.py
@@ -2,15 +2,24 @@ from flask import request
 from .. import db
 from marshmallow import ValidationError
 from ..schema.UserSchema import UserSchema,userSchemaMany
-from models.UserModel import User 
+from ..models.UserModel import User 
 
-def addExpense():
+def listUser():
+    data=User.query.all()
+    if data:
+        return{"data":userSchemaMany.dump(data)}
+    return{"message":"No record found"}
+
+def addBalance():
     data=request.json
     try:
-        valid = UserSchema.load(data)
+        valid = UserSchema().load(data)
         res = User(
             user_name=valid['user_name'],
             balance= valid['balance']
         )
+        db.session.add(res)
+        db.session.commit()
+        return{"message":"Balance added successfully","data:":res},200
     except ValidationError as err:
         return {"message":err.messages},422
