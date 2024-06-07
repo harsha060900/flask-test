c1cf5d92fa3ead8b0b0de6486e5f38dff1ad4115
harshavardhan-ngp <harshavardhan@ngpwebsmart.com>
2024-06-07 19:31:50 +0530

    expense balance update

--- src/views/expenseView.py
+++ src/views/expenseView.py
@@ -3,9 +3,12 @@ from .. import db
 from ..models.ExpenseModel import Expense
 from ..models.CateModel import Category
 from ..models.SubCateModel import SubCategory
+from ..models.UserModel import User
 from ..schema.ExpenseSchema import ExpenseSchema, expSchemaMany
+from ..schema.UserSchema import userSchemaMany
 from marshmallow import ValidationError
 from datetime import datetime
+from sqlalchemy import func
 
 # code from AI for between
 # python
@@ -28,14 +31,18 @@ def listExpense():
     orderBy=request.args.get('orderBy')
     start,end=request.args.get('start'),request.args.get('end')
     res=[]
+    userData= User.query.with_entities(User.balance).filter(User.id==1).all()
+    balance = userSchemaMany.dump(userData)[0]['balance']
     data=data=db.session.query(Expense, Category.cate_name, SubCategory.sub_cate_name).outerjoin(Category, Expense.cate_id == Category.id).outerjoin(SubCategory, Expense.sub_cate_id == SubCategory.id)
+    TotInc=Expense.query.with_entities(func.sum(Expense.amt)).filter(Expense.type=="income").first()[0]
+    TotExp=Expense.query.with_entities(func.sum(Expense.amt)).filter(Expense.type=="expense").first()[0]
     if orderBy == "asc" and not end:
-        data=data.order_by(Expense.created.asc()).all()
+        data=data.order_by(Expense.period.asc()).all()
     elif start and end:
         print('ssss:',start,end)
-        data=data.filter(Expense.period.between(start,end)).order_by(Expense.created.desc()).all()
+        data=data.filter(Expense.period.between(start,end)).order_by(Expense.period.desc()).all()
     else:
-        data=data.order_by(Expense.created.desc()).all()
+        data=data.order_by(Expense.period.desc()).all()
     for expense, cate_name, sub_cate_name in data:
         serialize = {
             'id': expense.id,
@@ -51,12 +58,13 @@ def listExpense():
         }
         res.append(serialize)
     # return{"data:":expSchemaMany.dump(data)},200
-    return{"data":res},200
+    return{"data":res,'balance':balance, 'totInc':TotInc,'totExp':TotExp},200
 
 def addExpense():
     data=request.json
     # start_time = datetime.strptime(data["period"], '%Y-%m-%d %H:%M:%S')
     # print('ss:',start_time)
+    userData= User.query.filter(User.id==1).first()
     try:
         valid=ExpenseSchema().load(data)
         res= Expense(
@@ -67,6 +75,10 @@ def addExpense():
             desc=valid["desc"],
             type=valid["type"]
         )
+        if valid['type']=='income':
+            userData.balance += valid['amt']
+        elif valid['type']=='expense':
+            userData.balance -= valid['amt']
         db.session.add(res)
         db.session.commit()
         return{"message":"Expense added successfully", "data":res}
@@ -76,10 +88,13 @@ def addExpense():
 def editExpense(paramId):
     reqData=request.json
     data=Expense.query.get(paramId)
-    # start_time = datetime.strptime(data["period"], '%Y-%m-%d %H:%M:%S')
-    # print('ss:',start_time)
+    userData= User.query.filter(User.id==1).first()
     try:
         valid=ExpenseSchema().load(reqData)
+        if valid['type']=='income':
+            userData.balance -= data.amt - valid['amt']
+        elif valid['type']=='expense':
+            userData.balance += data.amt - valid['amt']
         data.cate_id=valid["cate_id"],
         data.sub_cate_id=valid["sub_cate_id"],
         data.amt=valid["amt"],
@@ -92,8 +107,14 @@ def editExpense(paramId):
     
 def deleteExpense(paramId):
     data= Expense.query.get(paramId)
+    print("D:", data)
     if not data:
         return {"message":"No records found"},200
+    userData= User.query.filter(User.id==1).first()
+    if data.type=='income':
+        userData.balance -= data.amt
+    elif data.type=='expense':
+        userData.balance += data.amt
     db.session.delete(data)
     db.session.commit()
     return {"message":"Expense deleted successfully","data":data},200
